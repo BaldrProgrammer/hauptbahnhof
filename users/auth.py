82dfdf1b531cccc7d045468cc37132823e0bd492
balddrAdmin <boliklevik@gmail.com>
2026-01-23 22:49:24 +0100

    jwt_decode()

--- users/auth.py
+++ users/auth.py
@@ -14,3 +14,8 @@ async def jwt_encode(data: dict):
     expire_date = datetime.now(timezone.utc) + timedelta(days=1)
     data.update({'exp': expire_date})
     return jwt.encode(data, key='hauptbahnhof', algorithm='HS256')
+
+
+async def jwt_decode(token: str):
+    data = jwt.decode(token, key='hauptbahnhof', algorithms='HS256')
+    return data
