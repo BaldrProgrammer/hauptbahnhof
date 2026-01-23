1f3b68e035f56a5eb096078a35c7c29d92197398
balddrAdmin <boliklevik@gmail.com>
2026-01-23 23:43:29 +0100

    password hashing when register

--- users/auth_router.py
+++ users/auth_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Response, status
 from fastapi.exceptions import HTTPException
+from users.auth import jwt_encode, get_hash_password, verify_password
 from users.models import User
 from users.schemas import SUserReg, SUserLog
 
@@ -11,16 +12,12 @@ router = APIRouter(prefix='/auth')
 @router.post('/register')
 async def register(user: SUserReg):
     if not select(User, filter_by={'username': user.username}):
+        user.password = await get_hash_password(user.password)
         new_user = User(**user.model_dump(), tickets='{}')
         insert(new_user)
         return {'ok': True}
+
     return HTTPException(
         status.HTTP_409_CONFLICT,
         'пользователь уже существует'
     )
-
-
-@router.post('/login')
-async def login(auth_data: SUserLog, response: Response):
-    if user := select(User, filter_by={'username': auth_data.username}):
-        return {'ok': True}
