e54d50f0459c3d3eab8e9d97d628430a8fbaf563
balddrAdmin <boliklevik@gmail.com>
2026-01-29 23:44:57 +0100

    GET /relief/current

--- relief/router.py
+++ relief/router.py
@@ -1,3 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from relief.models import Relief
+from relief.schemas import SReliefGet
+from users.schemas import SUserGet
+from users.auth import get_current_user
+
+from bezdarsql import select
 
 router = APIRouter(prefix='/relief', tags=['/relief'])
+
+
+@router.get('/current')
+async def current_relief(user: SUserGet = Depends(get_current_user)) -> SReliefGet | None:
+    print(user)
+    if user:
+        relief = select(Relief, filter_by={'id': user.ulga})[0]
+        return relief
+    return None
