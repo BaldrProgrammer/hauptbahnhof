3b859d1342cd6258086ccab910b5aa4d53425699
balddrAdmin <boliklevik@gmail.com>
2026-01-29 23:18:21 +0100

    relief_router init

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from users.auth_router import router as auth_router
 from users.router import router as users_router
+from relief.router import router as relief_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(users_router)
+app.include_router(relief_router)
 
 app.add_middleware(
     CORSMiddleware,
