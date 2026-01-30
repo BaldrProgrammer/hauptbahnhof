23131e246fb55045b8a2ae65f573cd463c8a8640
balddrAdmin <boliklevik@gmail.com>
2026-01-30 12:27:42 +0100

    POST reliefs/add

--- relief/router.py
+++ relief/router.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from relief.models import Relief
-from relief.schemas import SReliefGet
+from relief.schemas import SReliefGet, SReliefAdd
 from users.schemas import SUserGet
 from users.auth import get_current_user
 
-from bezdarsql import select
+from bezdarsql import select, insert
 
 router = APIRouter(prefix='/relief', tags=['/relief'])
 
@@ -17,3 +17,12 @@ async def current_relief(user: SUserGet = Depends(get_current_user)) -> SReliefG
         relief = select(Relief, filter_by={'id': user.ulga})[0]
         return relief
     return None
+
+
+@router.post('/add')
+async def add_relief(new_relief: SReliefAdd, user: SUserGet = Depends(get_current_user)) -> dict:
+    if user.role == 'admin':
+        relief = Relief(**new_relief.model_dump())
+        insert(relief)
+        return {'ok': True, 'new_instance': select(Relief, filter_by={'title': new_relief.title})}
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You are not admin and not followed to create reliefs.')
