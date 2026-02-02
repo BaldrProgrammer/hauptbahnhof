963c95b58de70f418ee9dd3220dcda49967053ae
balddrAdmin <boliklevik@gmail.com>
2026-02-02 23:50:50 +0100

    GET stations/{station_id}(that one before was initialisating router)

--- stations/router.py
+++ stations/router.py
@@ -1,4 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from bezdarsql import select
+from stations.models import Station
 
 router = APIRouter(prefix='/stations', tags=['/stations'])
 
+
+@router.get('/{station_id}')
+async def get_station_by_id(station_id: int):
+    station = select(Station, filter_by={Station.id: station_id})
+    if station:
+        return select(Station, filter_by={Station.id: station_id})
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='station not found.')
