42b7b81597b079e595424acc9700579ee41d26ca
balddrAdmin <boliklevik@gmail.com>
2026-02-02 23:39:04 +0100

    SStation schema

--- stations/schemas.py
+++ stations/schemas.py
@@ -1,1 +1,12 @@
-from pydantic
+from pydantic import BaseModel
+
+
+class SStation(BaseModel):
+    id: int
+    title: str
+    city: str
+    region: str
+    country: str
+    address: str
+    coordinates: str
+    international_codes: str
