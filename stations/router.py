7223d53de13c2a1d35507ad0eaebc7c243f9ad0d
balddrAdmin <boliklevik@gmail.com>
2026-02-02 23:58:57 +0100

    GET stations/

--- stations/router.py
+++ stations/router.py
@@ -5,9 +5,14 @@ from stations.models import Station
 router = APIRouter(prefix='/stations', tags=['/stations'])
 
 
+@router.get('/')
+async def get_all_stations():
+    return select(Station, value='*')
+
+
 @router.get('/{station_id}')
 async def get_station_by_id(station_id: str):
     station = select(Station, filter_by={Station.id: station_id})
     if station:
-        return select(Station, filter_by={Station.id: station_id})
+        return station
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='station not found.')
