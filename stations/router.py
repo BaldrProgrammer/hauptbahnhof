47c6ecd95590add9af91ee687df1bc6a982fcb38
balddrAdmin <boliklevik@gmail.com>
2026-02-05 00:27:34 +0100

    GET stations/distance

--- stations/router.py
+++ stations/router.py
@@ -3,6 +3,7 @@ from bezdarsql import select, insert, delete
 from users.auth import get_current_user
 from stations.models import Station
 from stations.schemas import SStation
+from probnik import get_distance_km
 
 router = APIRouter(prefix='/stations', tags=['/stations'])
 
@@ -10,6 +11,16 @@ router = APIRouter(prefix='/stations', tags=['/stations'])
 @router.get('/')
 async def get_all_stations():
     return select(Station, value='*')
+
+
+@router.get('/distance')
+async def get_distance_between(stations: str):
+    ids = stations.split(',')
+    lat1, lon1 = select(Station, filter_by={Station.id: ids[0]})[0].coordinates.split(', ')
+    lat2, lon2 = select(Station, filter_by={Station.id: ids[1]})[0].coordinates.split(', ')
+
+    distance = get_distance_km(float(lat1), float(lon1), float(lat2), float(lon2))
+    return {'raw': distance, 'countable': round(distance)}
 
 
 @router.get('/{station_id}')
