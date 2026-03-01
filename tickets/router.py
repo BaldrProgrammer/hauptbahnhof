ff1f85f9228b519eb3109fa4ef994f41cfff8c4f
balddrAdmin <boliklevik@gmail.com>
2026-03-01 23:52:13 +0100

    GET /tickets/km_price

--- tickets/router.py
+++ tickets/router.py
@@ -1,3 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from users.schemas import SUserGet
+from users.auth import get_current_user
 
 router = APIRouter(prefix='/tickets')
+
+
+@router.get('/km_price')
+async def get_price_from_km(kilometers: float, user: SUserGet = Depends(get_current_user)):
+    if kilometers < 40:
+        price = 15
+    else:
+        price = 15 + (kilometers-40)/4
+    return price
