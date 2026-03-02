948c057e41fe1caa4634cf61f5a3bcd223e652e0
balddrAdmin <boliklevik@gmail.com>
2026-03-02 21:54:18 +0100

    reliefs 401 handler upgrade

--- reliefs/router.py
+++ reliefs/router.py
@@ -20,31 +20,37 @@ async def get_relief_by_id(relief_id: int):
 
 @router.post('/add')
 async def add_relief(new_relief: SReliefAdd, user: SUserGet = Depends(get_current_user)) -> dict:
-    if user.role == 'admin':
-        relief = Relief(**new_relief.model_dump())
-        insert(relief)
-        return {'ok': True, 'new_instance': select(Relief, filter_by={Relief.title: new_relief.title})}
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                        detail='You are not admin and not followed to create reliefs.')
+    if user:
+        if user.role == 'admin':
+            relief = Relief(**new_relief.model_dump())
+            insert(relief)
+            return {'ok': True, 'new_instance': select(Relief, filter_by={Relief.title: new_relief.title})}
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='You are not admin and not followed to create reliefs.')
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User is not authorised.')
 
 
 @router.patch('/update')
 async def update_relief(relief_id: str, row: str, value: str, user: SUserGet = Depends(get_current_user)):
-    if user.role == 'admin':
-        if select(Relief, filter_by={Relief.id: relief_id}):
-            update(Relief, values={getattr(Relief, row): value}, where={Relief.id: relief_id})
-            return {'ok': True}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Relief not found.')
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                        detail='You are not admin and not followed to change reliefs')
+    if user:
+        if user.role == 'admin':
+            if select(Relief, filter_by={Relief.id: relief_id}):
+                update(Relief, values={getattr(Relief, row): value}, where={Relief.id: relief_id})
+                return {'ok': True}
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Relief not found.')
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='You are not admin and not followed to change reliefs')
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User is not authorised.')
 
 
 @router.delete('/remove/{relief_id}')
 async def remove_relief_by_id(relief_id: int, user: SUserGet = Depends(get_current_user)) -> dict:
-    if user.role == 'admin':
-        if select(Relief, filter_by={Relief.id: relief_id}):
-            delete(Relief, where={Relief.id: relief_id})
-            return {'ok': True}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'relief id={relief_id} doesn\'t exists.')
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                        detail='You are not admin and not followed to create reliefs.')
+    if user:
+        if user.role == 'admin':
+            if select(Relief, filter_by={Relief.id: relief_id}):
+                delete(Relief, where={Relief.id: relief_id})
+                return {'ok': True}
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'relief id={relief_id} doesn\'t exists.')
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='You are not admin and not followed to create reliefs.')
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User is not authorised.')
