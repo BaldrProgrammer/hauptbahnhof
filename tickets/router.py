6e91f670bfcea968783dea6c2241bbe652cc5a6f
balddrAdmin <boliklevik@gmail.com>
2026-03-02 21:43:41 +0100

    tickets 401 handler upgrade

--- tickets/router.py
+++ tickets/router.py
@@ -7,8 +7,10 @@ router = APIRouter(prefix='/tickets', tags=['/tickets'])
 
 @router.get('/km_price')
 async def get_price_from_km(kilometers: float, user: SUserGet = Depends(get_current_user)):
-    if kilometers < 40:
-        price = 15
-    else:
-        price = 15 + (kilometers-40)/4
-    return (price / 100) * (100-user.ulga.discount)
+    if user:
+        if kilometers < 40:
+            price = 15
+        else:
+            price = 15 + (kilometers-40)/4
+        return (price / 100) * (100-user.ulga.discount)
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User is not authorised.')
